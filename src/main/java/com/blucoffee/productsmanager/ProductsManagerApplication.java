3b8158c761d3cd7c44471e4f37682f1262625875
Matheus Fernandino <mat.fernandino@gmail.com>
2024-06-20 19:55:13 -0300

    imports

--- src/main/java/com/blucoffee/productsmanager/ProductsManagerApplication.java
+++ src/main/java/com/blucoffee/productsmanager/ProductsManagerApplication.java
@@ -2,6 +2,8 @@ package com.blucoffee.productsmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
