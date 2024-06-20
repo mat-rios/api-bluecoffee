dd80cbeb2ee6a8074f340c230dbe491740621a96
Matheus Fernandino <108854732+mat-rios@users.noreply.github.com>
2024-06-20 19:52:37 -0300

    Update ProductsManagerApplication.java

--- src/main/java/com/blucoffee/productsmanager/ProductsManagerApplication.java
+++ src/main/java/com/blucoffee/productsmanager/ProductsManagerApplication.java
@@ -3,8 +3,14 @@ package com.blucoffee.productsmanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@RestController
 @SpringBootApplication
 public class ProductsManagerApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Rest BlueCoffeeApi";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductsManagerApplication.class, args);
